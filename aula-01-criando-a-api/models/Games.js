e443dcace5da9ca761b062279ad28f60a3430f10
DSM3 <fatec-dsm3@academico.intra>
2026-03-02 19:30:12 -0300

    puxando os dados do banco e tratando requisição via API

--- aula-01-criando-a-api/models/Games.js
+++ aula-01-criando-a-api/models/Games.js
@@ -1,12 +1,12 @@
 import mongoose from "mongoose";
 
-const gameChema = new mongoose.Schema({
+const gameSchema = new mongoose.Schema({
     title: String,
     platform: String,
     year: Number,
     price: Number
 })
 
-const Game = mongoose.model('Game', gameChema) // nome da coleção que eu quero criar no meu banco ('Game')
+const Game = mongoose.model('Game', gameSchema) // nome da coleção que eu quero criar no meu banco ('Game')
 
 export default Game;
